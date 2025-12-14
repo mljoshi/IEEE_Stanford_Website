c75c13356adf69273515121c23baa43a64caba37
mljoshi <mljoshiro@gmail.com>
2025-12-13 17:49:44 -0800

    Add hamburger menu for mobile

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { Link, useLocation } from 'react-router-dom'
+import Hamburger from 'hamburger-react'
 
 function NavLink({ to, children }) {
   const loc = useLocation()
@@ -17,6 +18,7 @@ function NavLink({ to, children }) {
 export default function Nav() {
   const [hidden, setHidden] = useState(false)
   const [lastY, setLastY] = useState(0)
+  const [mobileOpen, setMobileOpen] = useState(false)
   useEffect(() => {
     const onScroll = () => {
       const y = window.scrollY || 0
@@ -34,17 +36,29 @@ export default function Nav() {
   return (
     <header className={`navbar ${hidden ? 'navbar-hidden' : ''}`}>
     <div className="navbar-inner">
-      <Link to="/" className='navbar-logo'>
-        <img src={`${import.meta.env.BASE_URL}img/ieee.png`} className="navbar-logo-img" alt="IEEE Logo" />
-        <div className="navbar-logo-text">IEEE @ Stanford</div>
-      </Link>
-      <nav className="navbar-menu">
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/officers">Officers</NavLink>
-        <NavLink to="/events">Events</NavLink>
-        <NavLink to="/resources">Resources</NavLink>
-        <NavLink to="/contact">Contact</NavLink>
-      </nav>
+        <Link to="/" className='navbar-logo'>
+          <img src={`${import.meta.env.BASE_URL}img/ieee.png`} className="navbar-logo-img" alt="IEEE Logo" />
+          <div className="navbar-logo-text">IEEE @ Stanford</div>
+        </Link>
+        <nav className="navbar-menu hidden md:flex">
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/officers">Officers</NavLink>
+          <NavLink to="/events">Events</NavLink>
+          <NavLink to="/resources">Resources</NavLink>
+          <NavLink to="/contact">Contact</NavLink>
+        </nav>
+      <div className="md:hidden">
+        <Hamburger toggled={mobileOpen} toggle={setMobileOpen} />
+        {mobileOpen &&
+          <nav className="absolute top-full left-0 right-0 flex flex-col gap-2 px-6 py-4 bg-white border-t border-gray-200" onClick={() => setMobileOpen(false)}>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/officers">Officers</NavLink>
+            <NavLink to="/events">Events</NavLink>
+            <NavLink to="/resources">Resources</NavLink>
+            <NavLink to="/contact">Contact</NavLink>
+          </nav>
+        }
+      </div>
     </div>
   </header>
   )
